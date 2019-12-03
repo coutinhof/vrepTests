647747d38db39085d81e7cadab2894b0043ffef4
fcoutinho <tico.fernando@gmail.com>
2019-12-03 17:09:04 -0300

    Primeiro funcionamento da solda

--- matlab/kr90JointPositionTest.m
+++ matlab/kr90JointPositionTest.m
@@ -20,13 +20,14 @@ end
 KR90=kr90_mdl;
 
 initOri=[1 0 0;0 -1 0;0 0 -1];
-initPos=[0.3;1.9;0.92]
+initPos=[0.3;1.9;1]
 n0=KR90.ikine([initOri,initPos;0 0 0 1]);
 setJoints(clientID,handle_motor,vrep,n0);
 T=KR90.fkine(n0);
-pause(10);
-trajPosX1=0.3:-0.01:-0.3;
-trajPosY1=1.9:-0.01:1.7;
+pause(2);
+trajPosX1=0.3:-0.005:-0.3;
+trajPosY1=1.9:-0.005:1.7;
+for k=1:50
 for i=1:size(trajPosX1,2)
     T(1,4)=trajPosX1(i);
     nq=KR90.ikine(T,n0);
@@ -39,7 +40,7 @@ for i=1:size(trajPosY1,2)
     setJoints(clientID,handle_motor,vrep,nq)
     pause(0.1)
 end
-
+end
     else
         disp('Failed connecting to remote API server');
     end
